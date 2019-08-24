28a74cb52c498cd604face39eaeb048b801608e0
guozhenqiang <guozhenqiang@autohome.com.cn>
2019-08-24 22:35:51 +0800

    APod init

---
+++ APod/A_UIView.h
@@ -0,0 +1,17 @@
+//
+//  A_UIView.h
+//  APod
+//
+//  Created by Gorry on 2019/8/24.
+//  Copyright © 2019年 Gorry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface A_UIView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ APod/A_UIView.m
@@ -0,0 +1,20 @@
+//
+//  A_UIView.m
+//  APod
+//
+//  Created by Gorry on 2019/8/24.
+//  Copyright © 2019年 Gorry. All rights reserved.
+//
+
+#import "A_UIView.h"
+
+@implementation A_UIView
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.backgroundColor = UIColor.redColor;
+    }
+    return self;
+}
+
+@end
